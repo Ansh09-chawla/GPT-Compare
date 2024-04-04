d48c1c70077af5e921541cc1cb25290aeb77a707
Ansh09-chawla <ansh.jhande@gmail.com>
2024-04-04 01:06:40 -0600

    added frontend port# as env var

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: `http://localhost:${process.env.FRONTEND_PORT}` }));
 app.use(express.json());
 dotenv.config();
 // Route files
