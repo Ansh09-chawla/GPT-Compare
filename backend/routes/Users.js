85178b4348ac68d45288b75627e53e9cd802e387
sankim11 <davidsankim02@gmail.com>
2024-04-05 12:31:55 -0600

    update user

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { userLogin, createUser } from "../controllers/Users.js";
+import { userLogin, createUser, updateUser } from "../controllers/Users.js";
 
 const usersRoutes = express.Router();
 
 usersRoutes.post("/create-user", createUser);
 usersRoutes.post("/user-login", userLogin);
+usersRoutes.put("/user-update", updateUser);
 
 export default usersRoutes;
