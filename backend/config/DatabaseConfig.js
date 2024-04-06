be5fc8f7b69ad27a4e9edf515b5920caf51f8ff5
Aleksander Rudolf <aleks.rudolf@outlook.com>
2024-04-06 17:09:14 -0600

    Got Docker working

--- backend/config/DatabaseConfig.js
+++ backend/config/DatabaseConfig.js
@@ -5,7 +5,7 @@ const { Pool } = pg;
 
 const pgDatabase = new Pool({
 	user: process.env.POSTGRES_USER,
-	host: process.env.POSTGRES_HOST,
+	host: process.env.POSTGRES_HOST, // This matches the service name in the docker-compose file
 	database: process.env.POSTGRES_DB,
 	password: process.env.POSTGRES_PASSWORD,
 	port: process.env.POSTGRES_PORT,
