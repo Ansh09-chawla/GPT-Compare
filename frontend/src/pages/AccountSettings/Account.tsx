a9de5f8aa14a8163506d11bfe7075c332fe7e6d7
Johny Do <102762223+TienDatJohnyDo@users.noreply.github.com>
2024-04-10 12:14:55 -0600

    Cahnged button

--- frontend/src/pages/AccountSettings/Account.tsx
+++ frontend/src/pages/AccountSettings/Account.tsx
@@ -237,7 +237,7 @@ const AccountSettings = () => {
               onClick={handleSaveChanges}
               className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Save Changes
+              Save Account Changes
             </button>
           </div>
           {/* Username Modal */}
