17f5dbc539439c1d7bfb7dbb2d1d8246961a1d92
Johny Do <102762223+TienDatJohnyDo@users.noreply.github.com>
2024-04-10 14:15:13 -0600

    Finalized Account Settings Release1.0

--- frontend/src/pages/AccountSettings/Account.tsx
+++ frontend/src/pages/AccountSettings/Account.tsx
@@ -322,7 +322,7 @@ const AccountSettings = () => {
                   <button
                     type="button"
                     onClick={handleCloseEmailModal} 
-                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-White bg-0E153A hover:bg-0E153A focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-0E153A"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
                     Change
                   </button>
