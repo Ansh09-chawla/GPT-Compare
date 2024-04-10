ceef084022dd35ec40df25247fefdb3e9f9df83f
Johny Do <102762223+TienDatJohnyDo@users.noreply.github.com>
2024-04-10 12:11:16 -0600

    Changed to the account

---
+++ frontend/src/pages/AccountSettings/Account.tsx
@@ -0,0 +1,319 @@
+import React, { useEffect, useState } from "react";
+import { usersService } from "../../services/UsersService";
+import { jwtDecode } from "jwt-decode";
+
+const AccountSettings = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [showUsernameModal, setShowUsernameModal] = useState(false); // State for controlling the username modal
+  const [showEmailModal, setShowEmailModal] = useState(false); // State for controlling the email modal
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (token) {
+          const decoded = jwtDecode(token);
+          const userId = decoded.userId;
+          const response = await usersService.getUser(userId);
+
+          if (response.success) {
+            setUsername(response.user.username);
+            setEmail(response.user.email);
+            setRole(response.user.role);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setCurrentPassword("");
+    setNewPassword("");
+    setConfirmPassword("");
+  };
+
+  const handleOpenUsernameModal = () => {
+    setShowUsernameModal(true);
+  };
+
+  const handleCloseUsernameModal = () => {
+    setShowUsernameModal(false);
+  };
+
+  const handleOpenEmailModal = () => {
+    setShowEmailModal(true);
+  };
+
+  const handleCloseEmailModal = () => {
+    setShowEmailModal(false);
+  };
+
+  const handleChangePassword = async () => {
+    if (newPassword !== confirmPassword) {
+      alert("New password and confirm password do not match.");
+      return;
+    }
+
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        alert("No token found");
+        return;
+      }
+      const decoded = jwtDecode(token);
+      const userId = decoded.userId;
+
+      await usersService.changePassword(userId, currentPassword, newPassword);
+      alert("Password changed successfully");
+
+      setCurrentPassword("");
+      setNewPassword("");
+      setConfirmPassword("");
+      setShowModal(false);
+    } catch (error) {
+      console.error("Error changing password:", error);
+      alert("Error changing password");
+    }
+  };
+
+  const handleSaveChanges = async () => {
+    try {
+      const token = localStorage.getItem("token"); // to get the user id
+      if (!token) {
+        alert("No token found");
+        return;
+      }
+      const decoded = jwtDecode(token);
+      const id = decoded.userId;
+
+      if (!id) {
+        console.error("User ID not found in token");
+        return;
+      }
+
+      await usersService.updateUser(id, username, email, role);
+      alert("User updated successfully");
+    } catch (error) {
+      console.error("Error updating user:", error);
+      alert("Error updating user");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-6">
+      <div className="w-full max-w-xl p-8 space-y-6 rounded-lg bg-white shadow-lg font-inter">
+        <h2 className="text-left text-2xl font-bold text-gray-900">
+          Account Settings
+        </h2>
+        <br />
+        <form className="space-y-4">
+          <div>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+              <h5 className="text-left text-xl font-bold text-gray-900">
+                Username
+              </h5>
+              <span
+                className="text-blue-500 cursor-pointer underline"
+                onClick={handleOpenUsernameModal}
+                style={{ textDecoration: "underline" }} 
+              >
+                Change
+              </span>
+            </div>
+            <br />
+            <p>
+              Your username is{" "}
+              <span style={{ fontWeight: "bold" }}>{username}</span>
+            </p>
+            <div className="border-b border-gray-300 my-4"></div>
+          </div>
+
+          <div>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+              <h5 className="text-left text-xl font-bold text-gray-900">
+                Email address
+              </h5>
+              <span
+                className="text-blue-500 cursor-pointer underline"
+                onClick={handleOpenEmailModal}
+                style={{ textDecoration: "underline" }} 
+              >
+                Change
+              </span>
+            </div>
+            <br />
+            <p>
+              Your email address is{" "}
+              <span style={{ fontWeight: "bold" }}>{email}</span>
+            </p>
+            <div className="border-b border-gray-300 my-4"></div>
+          </div>
+          <div>
+            <h5 className="text-left text-xl font-bold text-gray-900">Role</h5>
+            <br />
+            <p>
+              Your account is role is{" "}
+              <span style={{ fontWeight: "bold" }}>{role}</span>
+            </p>
+            <div className="border-b border-gray-300 my-4"></div>
+          </div>
+          <div>
+            <h5 className="text-left text-xl font-bold text-gray-900">
+              Password
+            </h5>
+            <br />
+            <div className="flex flex-wrap">
+              <div className="mb-4">
+                <label
+                  htmlFor="currentPassword"
+                  className="block text-sm font-normal text-black-700"
+                >
+                  <strong>Current Password:</strong>
+                </label>
+                <input
+                  type="password"
+                  id="currentPassword"
+                  className="mt-1 block w-30 px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                />
+              </div>
+              <div className="flex mb-4">
+                <div className="mr-4">
+                  <label
+                    htmlFor="newPassword"
+                    className="block text-sm font-normal text-black-700"
+                  >
+                    <strong> New Password:</strong>
+                  </label>
+                  <input
+                    type="password"
+                    id="newPassword"
+                    className="mt-1 block w-30 px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="confirmPassword"
+                    className="block text-sm font-normal text-black-700"
+                  >
+                    <strong>Confirm New Password:</strong>
+                  </label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    className="mt-1 block w-30 px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+            <button
+              type="button"
+              onClick={handleChangePassword}
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Save Password
+            </button>
+            <div className="border-b border-gray-300 my-4"></div>
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="button"
+              onClick={handleSaveChanges}
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Save Changes
+            </button>
+          </div>
+          {/* Username Modal */}
+          {showUsernameModal && (
+            <div className="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center lg:mr-[-250px]">
+              <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+              </div>
+              <div className="z-20 bg-white rounded-lg shadow-lg p-6 mx-auto max-w-md">
+                <div className="mb-4">
+                  <h3 className="text-lg font-bold mb-2">Change Username</h3>
+                  <input
+                    type="text"
+                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-end">
+                  <button
+                    type="button"
+                    onClick={handleCloseUsernameModal}
+                    className="mr-5 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-black bg-gray-300 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    onClick={handleCloseUsernameModal} // Closing the modal without any logic
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-White bg-0E153A hover:bg-0E153A focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-0E153A"
+                  >
+                    Change
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+          {/* Email Modal */}
+          {showEmailModal && (
+            <div className="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center lg:mr-[-250px]">
+              <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+              </div>
+              <div className="z-20 bg-white rounded-lg shadow-lg p-6 mx-auto max-w-md">
+                <div className="mb-4">
+                  <h3 className="text-lg font-bold mb-2">Change Email Address</h3>
+                  <input
+                    type="email"
+                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="flex justify-end">
+                  <button
+                    type="button"
+                    onClick={handleCloseEmailModal}
+                    className="mr-5 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-black bg-gray-300 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    onClick={handleCloseEmailModal} // Closing the modal without any logic
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-White bg-0E153A hover:bg-0E153A focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-0E153A"
+                  >
+                    Change
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AccountSettings;
