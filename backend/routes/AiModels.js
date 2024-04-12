77ecd9797bd42f5cc3ab832d728649d4a1cf9e53
Ansh09-chawla <ansh.jhande@gmail.com>
2024-04-11 21:43:35 -0600

    fixed the app

--- backend/routes/AiModels.js
+++ backend/routes/AiModels.js
@@ -1,14 +1,14 @@
 import express from "express";
 import {
-	getAllAIModels,
-	addAIModel,
-	deleteAIModel,
+  getAllAIModels,
+  addAIModel,
+  deleteAIModel,
 } from "../controllers/AiModels.js";
 import { verifyJwtToken } from "../middlewares/VerifyJwtToken.js";
 
 const aiModelsRoutes = express.Router();
 
-aiModelsRoutes.get("/", verifyJwtToken, getAllAIModels);
+aiModelsRoutes.get("/", getAllAIModels);
 aiModelsRoutes.post("/add", verifyJwtToken, addAIModel);
 aiModelsRoutes.delete("/delete/:modelId", verifyJwtToken, deleteAIModel);
 
--- frontend/src/utilities/AuthAxios.tsx
+++ frontend/src/utilities/AuthAxios.tsx
@@ -2,30 +2,31 @@ import axios from "axios";
 
 // Create an instance of axios
 const authAxios = axios.create({
-	baseURL:
-		import.meta.env.VITE_ENVIRONMENT === "prod"
-			? import.meta.env.VITE_API_URL_PROD
-			: import.meta.env.VITE_API_URL_DEV,
+  baseURL:
+    import.meta.env.VITE_ENVIRONMENT === "prod"
+      ? import.meta.env.VITE_API_URL_PROD
+      : import.meta.env.VITE_API_URL_DEV,
 });
 
 // Setup an interceptor to inject the token into headers of each request
 authAxios.interceptors.request.use(
-	(config) => {
-		// Retrieve the token from localStorage
-		const token = localStorage.getItem("token");
+  (config) => {
+    // Retrieve the token from localStorage
 
-		// If token is found, set the Authorization header
-		if (token) {
-			if (config.headers) {
-				config.headers["Authorization"] = `Bearer ${token}`;
-			}
-		}
+    const token = localStorage.getItem("token");
 
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
+    // If token is found, set the Authorization header
+    if (token) {
+      if (config.headers) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default authAxios;
